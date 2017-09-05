6dbbb1395d45165d6fd8b11e93a161d62286b3b4
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-05 15:53:30 +0530

    Create answer11.cpp

---
+++ answer11.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+  cout << "Enter the week day number";
+  cin >> num;
+  if(num==1)
+{
+         cout << "Weed day= Monday";
+}
+  else if(num==2)
+{
+         cout << "Week day= Tuesday";
+}
+ else if(num==3)
+{
+         cout << "Week day= Wednesday";
+}
+else if(num==4)
+{
+         cout << "Week day= Thursday";
+}
+else if(num==5)
+{
+         cout << "Week day= Friday";
+}
+else if(num==6)
+{
+         cout << "Week day= Saturday";
+}
+else if(num==7)
+{
+         cout << "Week day= Sunday";
+}
+else
+{
+    cout << "Error";
+}
+}
