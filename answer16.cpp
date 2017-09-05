c13e25e9ca2d5b14571c6d2a73a9fe2a756c3540
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-05 16:51:04 +0530

    Create answer16.cpp

---
+++ answer16.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float a,b,c;
+  cout << "Enter the three sides of the triangle:";
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  if(a==b && b==c && c==a)
+                cout << "The triangle is equilateral";
+  else if(a==b || b==c || c==a)
+                cout << "The triangle is isosceles";
+ else if(a!=b && b!=c && c!=a)
+        
