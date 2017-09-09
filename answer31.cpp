65be7c16c210e6e16a863ae1ba3f7c232b91fa79
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-09 18:37:27 +0530

    Create answer31.cpp

---
+++ answer31.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+           int n,i=0;
+           cout << "Enter the number:" << endl;
+	cin >> n;
+	while(n>0)
+	{
+                 n=n/10;
+               
+                
+	   i++;         
+		
+	}
+            cout << "The number of digits=" << i;
+	 	
+	
+}
