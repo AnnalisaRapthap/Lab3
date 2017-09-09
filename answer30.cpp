4a4f97d3bbd91f675bc24b42629651843b602b3f
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-09 18:25:52 +0530

    Create answer30.cpp

---
+++ answer30.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+           int n,i=0,p;
+           cout << "Enter the number whose multiplication table is to be displayed:" << endl;
+	cin >> n;
+	while(i<=12)
+	{
+                 p=n*i;
+                 cout << n << "*" << i << "=" << p << endl;
+                 i++;
+
+	            
+		
+	}
+            
+	 	
+	
+}
