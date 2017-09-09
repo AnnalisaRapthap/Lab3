b30c4424e460ad56f2dec87b7815d55d2f011d16
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-09 17:54:03 +0530

    Create answer22.cpp

---
+++ answer22.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i=1,s;
+	cout << "Enter the value of n" << endl;
+	cin >> n;
+	while(i<=n)
+	{
+	       
+	          
+	      cout << i << endl;
+	      i++;
+	}
+
+	 	
+	
+}
