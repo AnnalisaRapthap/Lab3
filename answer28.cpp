985b04680d542a410bd0129c535880808d1d07e0
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-09 18:19:29 +0530

    Create answer28.cpp

---
+++ answer28.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+           int n=1,s=0,i=2;
+	while(i<=100)
+	{
+		s=s+i;
+		i=i+2;
+
+	            
+		
+	}
+            cout << s;
+	 	
+	
+}
