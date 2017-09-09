cf585b409db48300ecc461c99c40c08be0e48f44
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-09 18:15:00 +0530

    Rename answer26.cpp to answer27.cpp

---
+++ answer27.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+           int n,s=0,i=1;
+           cout << "Enter the value of n" << endl;
+           cin >> n;
+	while(i<=n)
+	{
+		s=s+i;
+		i++;
+	            
+		
+	}
+            cout << s;
+	 	
+	
+}
