767713c5f6fe9954c6b869d8e6559acef31a89fb
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-12 17:18:37 +0530

    Create answer48.cpp

---
+++ answer48.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,j,c;
+cout << "Enter the value of n:";
+cin >> n;
+for(i=1;i<=n;i++)
+{
+c=0;
+for(j=1;j<=i;j++)
+{
+  if(i%j==0)
+
+         c++;
+
+}
+if(c==2)
+{
+cout << i << ",";
+}
+}
+}
