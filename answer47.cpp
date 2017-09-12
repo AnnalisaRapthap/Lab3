14962c45312ddf808c73352a5259a5647fd47d63
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-12 16:32:29 +0530

    Create answer47.cpp

---
+++ answer47.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,j=0;
+cout << "Enter the number:";
+cin >> n;
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+j++;
+}
+}
+if(j==2)
+{
+cout << "The number is a prime number" << endl;
+}
+else
+{
+cout << "The number is not a prime number";
+}
+}
