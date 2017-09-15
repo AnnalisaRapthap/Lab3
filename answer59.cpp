3c6bed15d9ce0a24d00a536b323f8bbc89a3880f
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2017-09-15 09:45:29 +0530

    Create answer59.cpp

---
+++ answer59.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+/*PROGRAM TO PRINT 2'S COMPLEMENT OF A NUMBER*/
+
+int main()
+{
+        int len,i;
+        char bin[17],twos[17];
+        int first=0;
+        cout<<"Enter binary number (maximum 16 bits): " << endl;
+        cin>>bin;
+        for(len=0;bin[len]!='\0';len++);
+        for( i=(len-1) ; i>=0 ; i-- )
+        {
+                if(first==0)
+               {
+                        if(bin[i]=='0')
+                        {
+                                twos[i]='0';
+                        }
+                        else
+                        {
+                                twos[i]='1';
+                                first=1;
+                         }
+                }
+                else
+                {
+                         if(bin[i]=='0')
+                         {
+                                 twos[i]='1';
+                         }
+                         else
+                         {
+                                 twos[i]='0';
+                         }
+               }
+        }
+ 
+        cout<<"Two's compliment: "<<twos;
+ 
+        return 0;
+}
